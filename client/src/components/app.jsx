10a84ca7dd76e1bc4cf1510e31f66b9ad5594d62
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-12-25 03:54:10 -0800

    connect graph with user data, add seperate bars for K/D/A

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -3,7 +3,7 @@ import * as d3 from 'd3'
 //import node from './d3test.jsx'
 //import * as rd3 from 'react-d3-library';
 //const RD3Component = rd3.Component;
-let width = 400;
+let width = 600;
 let height = 500;
 let i = 0;
 let margin = {left: 20, right: 20, top: 20, right:20}
@@ -12,45 +12,79 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      data:[1,2,3,4,5,6,7,8,9],
-      bars:[]
+      data:[{stats:{kills:1, deaths:2, assists:3}}],
+      killBars:[],
+      deathBars:[],
+      assistBars:[],
     }
   }
 
-  getData(){
-    let data = this.state.data
+  setBars(data, type){
+
+    // data = this.state.data
+
     const xScale = d3.scaleLinear().range([0, width - 100])
     const yScale = d3.scaleLinear().range([height, 0])
-    //const colorScale = d3.scaleSequential(d3.interpolateSpectral)
-
     const domain = d3.extent(data, (d, i) => i); //[1,8]
-    console.log(domain)
-    const val = d3.max(data, d => d); //8
-    //const [minAvg, maxAvg] = d3.extent(data, d => d.avg);
+    //console.log(domain)
+    const val = d3.max(data, d => d.stats.kills + d.stats.deaths + d.stats.assists);
+
     xScale.domain(domain);
     yScale.domain([0, val]);
-    //colorScale.domain([maxAvg, minAvg]);
 
-    // calculate x and y for each rectangle
-    return data.map((d, i)=> {
+    let result = data.map((d, i)=> {
+      const y1 = yScale(d.stats[type]);
+      let y;
+      let filler;
 
-      const y1 = yScale(d);
+      if(type === 'kills'){
+        y = y1;
+        filler = 'green';
+      }else if(type === 'deaths'){
+        y=y1 - (height - yScale(d.stats.kills));
+        filler = 'red';
+      }else if(type === 'assists'){
+        y=y1 - (height - yScale(d.stats.kills)) - (height - yScale(d.stats.deaths));
+        filler = 'orange';
+      }
+
       return {
         x: xScale(i),
-        y: y1,
-        width: width / data.length,
+        y: y,
+        width: (width - margin.left - margin.right - 30)  / data.length,
         height: height - y1,
-        fill: 'black',//colorScale(d.avg),
+        fill: filler,//colorScale(d.avg),
         stroke: 'white',
       }
     });
-
+    return result;
   }
 
+  setKillBars(){
+    let killBars = this.setBars(this.state.data, 'kills')
+    this.setState({killBars})
+  }
+
+  setDeathBars(){
+    let deathBars = this.setBars(this.state.data, 'deaths')
+    this.setState({deathBars})
+  }
+
+  setAssistBars(){
+    let assistBars = this.setBars(this.state.data, 'assists')
+    this.setState({assistBars})
+  }
+
+  setAllBars(){
+    this.setAssistBars();
+    this.setDeathBars();
+    this.setKillBars();
+  }
+
+
   componentDidMount(){
-    let bars = this.getData();
+    this.setAllBars();
     //console.log(bars)
-    this.setState({bars: bars})
     this.getUserData('thegnardogg')
   }
 
@@ -59,7 +93,8 @@ class App extends React.Component {
     .then(data => data.json())
     .then(data => {
       let userStats = data.map(match => this.getParticipantData(userName, match))
-      console.log(userStats);
+      this.setState({data:userStats})
+      this.setAllBars();
     })
   }
 
@@ -90,7 +125,13 @@ class App extends React.Component {
   render(){
     return (
       <svg width={width} height={height}>
-        {this.state.bars.map(d => (
+        {this.state.killBars.map(d => (
+          <rect x={d.x} y={d.y} width={d.width} height={d.height} fill={d.fill} stroke={d.stroke} key={i++}/>
+        ))}
+        {this.state.deathBars.map(d => (
+          <rect x={d.x} y={d.y} width={d.width} height={d.height} fill={d.fill} stroke={d.stroke} key={i++}/>
+        ))}
+        {this.state.assistBars.map(d => (
           <rect x={d.x} y={d.y} width={d.width} height={d.height} fill={d.fill} stroke={d.stroke} key={i++}/>
         ))}
 
